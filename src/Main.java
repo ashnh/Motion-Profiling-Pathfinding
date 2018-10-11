cdce3ccf4ed9da05b4f3b1e217e64fb2161df467
ashnh <ashnhx@gmail.com>
2018-10-11 16:17:13 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	int HEIGHT = 3;
+	int LENGTH = 3;
+	
+	public static int[][] allPaths () {
+		
+		int[][] paths;
+		
+		
+		
+		
+		return paths;
+		
+	}
+	
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int graph[][] = new int[][]{ 
+            						{1, 0, 0}, 
+            						{0, 0, 0},
+            						{0, 0, 2}
+           }; 
+
+	}
+
+}

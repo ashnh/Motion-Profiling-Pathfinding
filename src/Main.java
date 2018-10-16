5d8d5b6894c8a2bc279d34abc08f80f6bcdb5a5a
ashnh <ashnhx@gmail.com>
2018-10-15 22:03:41 -0400

    *oof noises*

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 
 public class Main {
 
-	
-	public Main() {
-		
-		
-		
-	}
+	public static final double Inches_Per_Second_Of_Power = 12; 
 
 	public static void main(String[] args) {
 		
 		Drive drive = new Drive ();
 		
+		// maxSpeed, acceleration, total distance
+		VelocityTrapezoid vt = new VelocityTrapezoid (1D, 0.1D, 100D);
 		
-		
+		// currentDistance, time
+		System.out.println(vt.getError( 6.4D, 8D));
 		
 	}
 
--- src/VelocityTrapezoid.java
+++ src/VelocityTrapezoid.java
@@ -5,6 +5,7 @@ public class VelocityTrapezoid {
 	private double accelerationSlope;
 	private double distance;
 	private double accelerationTime;
+	private double expectedTime;
 	
 	public VelocityTrapezoid(double _maxSpeed, double _accelerationSlope, double _distance) {
 		
@@ -12,9 +13,18 @@ public class VelocityTrapezoid {
 		accelerationSlope = _accelerationSlope;
 		accelerationTime = maxSpeed / accelerationSlope;
 		distance = _distance;
+		expectedTime = ((distance - (accelerationSlope * Math.pow(accelerationTime, 2))) / maxSpeed) + (2* accelerationTime);
 		
-		// d = .5at^2 + 
-		// 100 = 5
+		// Dm = D - 2De
+		// De = .5 * a(te^2)
+		// Dm = D - 2(.5 * a(te^2))
+		// Dm = Vm * tm
+		// Vm * tm = D - a(te^2)
+		// tm = (D - a(te^2)) / Vm
+		// total time = tm + 2te
+		
+		
+		// total time = (D - a(te^2)) / Vm + 2te
 		
 	}
 	
@@ -28,20 +38,21 @@ public class VelocityTrapezoid {
 	}
 	
 	
-	public double getError (double currentPower, double currentDistance, double encoderState, double time) {
+	public double getError ( double currentDistance, double time) {
 		
-		double currentVelocity = encoderState / time;
+		if (time == 0d)
+			return -131D;
+		
+		double currentVelocity = currentDistance / time;
 		
 		double expectedVelocity = 0D;
 		
-		if (currentDistance < distance - (accelerationTime * maxSpeed)) {
+		if (currentDistance < distance - (accelerationTime * maxSpeed))
 			expectedVelocity = accelerationSlope * time;
-			expectedVelocity = (expectedVelocity > maxSpeed) ? maxSpeed : expectedVelocity;
-		} else {
-			
-			//aaaaaaaaaaaaaAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHH
-			
-		}
+		 else 
+			expectedVelocity = (expectedTime - time) * accelerationSlope;
+		
+		expectedVelocity = (expectedVelocity > maxSpeed) ? maxSpeed : expectedVelocity;
 		
 		return (currentVelocity - expectedVelocity) / expectedVelocity;
 		
